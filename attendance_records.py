65b1c1e517002d42ca3440efbdd74731d24cf126
kzkpro <kishor.khambu@gmail.com>
2024-07-16 11:08:24 +0545

    updates

--- attendance_records.py
+++ attendance_records.py
@@ -1,12 +1,12 @@
-from student_record import Students
 import json
 import datetime
+from student_record import Students
 
 class Attendance:
     def __init__(self):
         self.students = Students()
         self.date = datetime.datetime.now().strftime("%Y-%m-%d")
-        self.attendance_records ={}
+        self.attendance_records = self.read_attendance_json()
 
     def write_attendance_json(self, data):
         try:
@@ -22,98 +22,95 @@ class Attendance:
             return data
         except FileNotFoundError:
             print("Attendance file not found. Creating a new one.")
+            self.write_attendance_json({})
             return {}
         except json.JSONDecodeError as e:
             print(f"Error decoding JSON: {e}")
-            return None
+            return {}
 
     def attendance_record(self):
         self.students.students_record = self.students.read_json()
 
-        if self.students.students_record is None:
+        if not self.students.students_record:
+            print("No student records found.")
             return
 
-        self.attendance_records = self.read_attendance_json()
-        self.day = datetime.datetime.now().strftime("%Y-%m_%d")
+        self.date = datetime.datetime.now().strftime("%Y-%m-%d")
+
+        if self.date in self.attendance_records:
+            print(f"Attendance for {self.date} is already recorded.")
+            return
+
+        attendance_record_for_day = {}
+
+        for key, student in self.students.students_record.get("students", {}).items():
+            while True:
+                registry = input(f"Is {student} present or absent? ").lower()
+                if registry in ["present", "absent"]:
+                    attendance_record_for_day[key] = {"student_id": student, "status": registry}
+                    break
+                else:
+                    print("Please enter 'present' or 'absent'.")
+
+        self.attendance_records[self.date] = attendance_record_for_day
+        self.write_attendance_json(self.attendance_records)
 
         while True:
-
-            self.tomorrow = datetime.datetime.now() + datetime.timedelta(days=1)
-
-            if self.attendance_records is None:
-                self.attendance_records = {}
-
-            if self.day in self.attendance_records:
-                print(f" Attendance of {self.day} is already recorded .")
+            to_continue = input("Do you want to continue the attendance for the next day? (yes/no) ").lower()
+            if to_continue == "yes":
+                self.date = (datetime.datetime.now() + datetime.timedelta(days=1)).strftime("%Y-%m-%d")
+                self.attendance_record()
+            elif to_continue == "no":
                 break
-
-
-
-            self.attendance_record_for_day = {}
-
-
-
-            for key in self.students.students_record.get("students" , {}).keys():
-                self.registry = input(f"Is {self.students.students_record['students'][key]} present or absent?\n")
-                if self.registry == "present".lower():
-                    self.attendance_record_for_day[key] = { "student_id "  : self.students.students_record["students"][key] , "present" : self.registry}
-                else:
-                    print("please enter 'present' or 'absent' on lower ")
-                    return
-
-
-            self.attendance_records[self.day] = list(self.attendance_record_for_day.values())
-
-            self.write_attendance_json(self.attendance_records)
-
-            self.to_continue = input("Do you want to continue the attendance for next day ? \n")
-            if self.to_continue == "yes".lower():
-                self.day = self.tomorrow.strftime("%Y-%m-%d")
-
             else:
-                break
+                print("Please enter 'yes' or 'no'.")
 
     def edit_attendance_record(self):
-        self.attendance_records = self.read_attendance_json()
         self.students.students_record = self.students.read_json()
 
         while True:
-            self.to_edit = input("Please enter the date of attendance history: ")
-            if self.to_edit in self.attendance_records.keys():
-                self.name_of_student = input("Please enter the name of the student: ")
-                if self.name_of_student in self.students.students_record.get("students", {}).values():
-                    self.change_attendance = input("Please enter 'present' or 'absent': ")
-                    for d in self.attendance_records[self.to_edit]:
-                        if d["student_id"] == self.name_of_student:
-                            d["present"] =  self.change_attendance.lower()
-                    self.write_attendance_json(self.attendance_records)
-                    break
+            date_to_edit = input("Please enter the date of the attendance record to edit (YYYY-MM-DD): ")
+            if date_to_edit in self.attendance_records:
+                student_name = input("Please enter the name of the student: ")
+                if student_name in self.students.students_record.get("students", {}).values():
+                    while True:
+                        new_status = input("Please enter 'present' or 'absent': ").lower()
+                        if new_status in ["present", "absent"]:
+                            for record in self.attendance_records[date_to_edit].values():
+                                if record["student_id"] == student_name:
+                                    record["status"] = new_status
+                                    self.write_attendance_json(self.attendance_records)
+                                    print("Attendance record updated.")
+                                    return
+                        else:
+                            print("Please enter 'present' or 'absent'.")
                 else:
                     print("Student not found. Please enter a valid student name.")
             else:
                 print("Date not found. Please enter a valid date.")
 
     def show_student_attendance(self):
-        date_input = input("Please enter the date of attendance date: ")
-        self.attendance_records = self.read_attendance_json()
-        for key, value in self.attendance_records.get(date_input, {}).items():
-            print(f"For date {key}, attendance records: {value}")
+        date_input = input("Please enter the date of attendance record to view (YYYY-MM-DD): ")
+        if date_input in self.attendance_records:
+            print(f"Attendance records for {date_input}:")
+            for student_id, record in self.attendance_records[date_input].items():
+                print(f"Student ID: {student_id}, Status: {record['status']}")
+        else:
+            print("No attendance record found for the given date.")
 
-
-
-
-def Student_attendance():
+def student_attendance():
     attendance = Attendance()
     while True:
-        user_input = input("Please Enter\n 1 . For Attendance \n 2 . To Edit Student Attendance \n 3 . To Show Attendance Record \n")
+        user_input = input("Please Enter\n 1. For Attendance \n 2. To Edit Student Attendance \n 3. To Show Attendance Record \n 4. To Exit\n")
         if user_input == "1":
             attendance.attendance_record()
         elif user_input == "2":
             attendance.edit_attendance_record()
         elif user_input == "3":
             attendance.show_student_attendance()
+        elif user_input == "4":
+            break
         else:
-            print("Input Error \n Please Enter 1 . For Attendance \n 2 . To Edit Student Attendance \n 3 . To Show Attendance Record")
-            Student_attendance()
+            print("Invalid input. Please enter 1, 2, 3, or 4.")
 
-Student_attendance()
+student_attendance()
